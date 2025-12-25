59abc79aa5a9de565df5b4bd1c49bd76518e279b
iurvish <urvishpvt@gmail.com>
2025-12-25 22:48:55 +0530

    feat: add `MorphCollections` component for animating different collection views using Framer Motion.

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Hero from "@/components/hero";
 import PhotoGallery from "@/components/todo/photo-gallery";
 import FeatureGallery from "@/components/todo/feature-gallery";
 import TabSwitchPhoto from "@/components/todo/tab-swtich-photo";
+import FolderInteraction from "@/registry/default/example/folder-interaction";
+
+import MorphCollections from "@/components/todo/morph-collections";
 
 export default function Page() {
   return (
     <main className="min-h-screen light bg-white font-[family-name:var(--font-geist-sans)]">
       <Hero />
-      <TabSwitchPhoto />
-      <PhotoGallery />
-      <FeatureGallery />
+      <div className="flex min-h-screen justify-center items-center flex-col gap-12 py-20">
+        <MorphCollections />
+        <FolderInteraction />
+      </div>
     </main>
   );
 }
