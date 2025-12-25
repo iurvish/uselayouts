d96e5f78e4e8474a0121f4fca98c61112c7d1238
iurvish <urvishpvt@gmail.com>
2025-12-25 15:20:43 +0530

    feat: add new page layout with Hero and interactive FeatureGallery components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Hero from "@/components/hero";
 import { ComponentExample } from "@/components/component-example";
 import PhotoGallery from "@/components/todo/photo-gallery";
+import FeatureGallery from "@/components/todo/feature-gallery";
 
 export default function Page() {
   return (
-    <main className="min-h-screen dark    font-[family-name:var(--font-geist-sans)]">
-      {/* <Hero /> */}
+    <main className="min-h-screen light bg-white    font-[family-name:var(--font-geist-sans)]">
+      <Hero />
       <div className="min-h-screen"></div>
       <div className="min-h-screen"></div>
       <PhotoGallery />
+      <FeatureGallery />
     </main>
   );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -53,12 +53,12 @@ const Hero = () => {
 
         <div className="relative z-10 w-full px-4 md:px-8 lg:pl-12 lg:pr-0 xl:pl-20 xl:pr-14 flex-1 flex flex-col justify-center  max-sm:px-2">
           <div className="flex flex-col justify-center gap-2 flex-1 lg:grid lg:grid-cols-2 lg:gap-12 lg:items-center max-sm:gap-12">
-            <div className="flex-1 flex flex-col gap-4 items-start justify-center max-md:items-center max-md:text-center text-left max-md:w-full  max-sm:justify-end ">
+            <div className="flex-1 flex flex-col gap-4 items-start justify-center max-lg:items-center max-lg:text-center text-left max-lg:w-full  max-sm:justify-end ">
               <div className="">
                 <h1 className=" tracking-tighter text-balance text-4xl font-medium md:text-5xl lg:text-6xl text-foreground ">
                   A micro-interaction UI library for professionals.
                 </h1>
-                <p className="mt-3 text-pretty text-lg max-md:text-center max-md:text-sm text-muted-foreground  max-sm:px-2">
+                <p className="mt-3 text-pretty text-lg max-lg:text-center max-md:text-sm text-muted-foreground  max-sm:px-2">
                   People don’t fall in love with components. They fall in love
                   with how something feels.
                 </p>
@@ -87,7 +87,7 @@ const Hero = () => {
               </div>
             </div>
 
-            <div className="flex-1 flex justify-end items-end lg:justify-end w-full h-full max-md:items-end max-md:pb-6 max-md:landscape:hidden">
+            <div className="flex-1 flex justify-end items-end lg:justify-end w-full h-full max-lg:items-center max-lg:justify-center max-md:pb-6 max-md:landscape:hidden">
               <div className="w-full max-w-[600px] lg:max-w-none">
                 <Bucket />
               </div>
