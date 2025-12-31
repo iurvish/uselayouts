f7a63bac00fe02a0c7d06a8de064a65b97be101b
iurvish <urvishpvt@gmail.com>
2025-12-31 23:18:34 +0530

    remove unneed files

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 import Hero from "@/components/hero";
 import ProductBenefits from "@/components/product-benefits";
-import ExpandableGallery from "@/registry/default/example/expandable-gallery";
-import FeatureCarousel from "@/registry/default/example/feature-carousel";
-import VerticalTabs from "@/registry/default/example/vertical-tabs";
-import LayoutSwitcher from "@/registry/default/example/animated-collection";
-import FolderInteraction from "@/registry/default/example/folder-interaction";
-import MorphCollections from "@/components/todo/morph-collections";
-import MultiStepForm from "@/components/todo/multistep-form";
-import ShakeTestimonial from "@/registry/default/example/shake-testimonial-card";
-import PricingCard from "@/registry/default/example/pricing-card";
 import Testimonial from "@/components/testimonial";
 import Footer from "@/components/footer";
 
@@ -18,8 +9,6 @@ export default function Page() {
       <Hero />
       <ProductBenefits />
       <Testimonial />
-
-      {/* <ExpandableGallery /> */}
 
       <Footer />
     </main>
